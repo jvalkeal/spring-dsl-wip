e3b06482a709e7cee5864f8881a150a6ae686348
Janne Valkealahti <janne.valkealahti@gmail.com>
2018-07-22 13:04:03 +0100

    Add base ngx monaco editor

--- spring-dsl-samples/simpledsleditor/ui/src/app/app.module.ts
+++ spring-dsl-samples/simpledsleditor/ui/src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { SpringDslEditorComponent } from './spring-dsl-editor/spring-dsl-editor.component';
+import { MonacoEditorModule, NgxMonacoEditorConfig } from 'ngx-monaco-editor';
+
+const monacoConfig: NgxMonacoEditorConfig = {
+  onMonacoLoad: () => {
+    console.log((<any>window).monaco);
+    (<any>window).monaco.languages.register({ id: 'simple' });
+  }
+};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SpringDslEditorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    MonacoEditorModule.forRoot(monacoConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
