4544fd76f0bad629a7e928aa186be7a9ca908f9e
Janne Valkealahti <janne.valkealahti@gmail.com>
2018-07-22 08:39:53 +0100

    reset to empty angular6 project

--- spring-dsl-samples/simpledsleditor/ui/src/app/app.module.ts
+++ spring-dsl-samples/simpledsleditor/ui/src/app/app.module.ts
@@ -1,29 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { Lsp4jComponent } from './lsp4j/lsp4j.component';
-import { MonacoEditorModule, NgxMonacoEditorConfig } from 'ngx-monaco-editor';
-
-const monacoConfig: NgxMonacoEditorConfig = {
-  // baseUrl: 'app-name/assets',
-  // defaultOptions: { scrollBeyondLastLine: false },
-  onMonacoLoad: () => {
-    console.log((<any>window).monaco);
-    (<any>window).monaco.languages.register({ id: 'simple' });
-  }
-};
 
 @NgModule({
   declarations: [
-    AppComponent,
-    Lsp4jComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule,
-    MonacoEditorModule.forRoot(monacoConfig)
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
