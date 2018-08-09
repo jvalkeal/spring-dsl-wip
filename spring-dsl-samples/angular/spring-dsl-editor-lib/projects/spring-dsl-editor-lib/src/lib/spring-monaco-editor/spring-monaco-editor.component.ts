51dae626c23d696df2cde217fbc6f20bd84ea06c
Janne Valkealahti <janne.valkealahti@gmail.com>
2018-08-09 11:56:20 +0100

    add new monaco editor component

---
+++ spring-dsl-samples/angular/spring-dsl-editor-lib/projects/spring-dsl-editor-lib/src/lib/spring-monaco-editor/spring-monaco-editor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'spring-spring-monaco-editor',
+  templateUrl: './spring-monaco-editor.component.html',
+  styleUrls: ['./spring-monaco-editor.component.css']
+})
+export class SpringMonacoEditorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- spring-dsl-samples/angular/spring-dsl-editor-lib/projects/spring-dsl-editor-lib/src/public_api.ts
+++ spring-dsl-samples/angular/spring-dsl-editor-lib/projects/spring-dsl-editor-lib/src/public_api.ts
@@ -5,3 +5,4 @@
 export * from './lib/spring-dsl-editor-lib.service';
 export * from './lib/spring-dsl-editor-lib.component';
 export * from './lib/spring-dsl-editor-lib.module';
+export * from './lib/spring-monaco-editor/spring-monaco-editor.component'
