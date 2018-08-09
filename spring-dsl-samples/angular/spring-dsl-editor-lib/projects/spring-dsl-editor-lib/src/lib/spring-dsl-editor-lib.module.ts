4b3c23b257be7c8a7acb323895d1c3053b4bb1f7
Janne Valkealahti <janne.valkealahti@gmail.com>
2018-08-09 11:56:20 +0100

    add new monaco editor component

--- spring-dsl-samples/angular/spring-dsl-editor-lib/projects/spring-dsl-editor-lib/src/lib/spring-dsl-editor-lib.module.ts
+++ spring-dsl-samples/angular/spring-dsl-editor-lib/projects/spring-dsl-editor-lib/src/lib/spring-dsl-editor-lib.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { SpringDslEditorLibComponent } from './spring-dsl-editor-lib.component';
+import { SpringMonacoEditorComponent } from './spring-monaco-editor/spring-monaco-editor.component';
 
 @NgModule({
   imports: [
   ],
-  declarations: [SpringDslEditorLibComponent],
-  exports: [SpringDslEditorLibComponent]
+  declarations: [
+    SpringDslEditorLibComponent,
+    SpringMonacoEditorComponent
+  ],
+  exports: [
+    SpringDslEditorLibComponent,
+    SpringMonacoEditorComponent
+  ]
 })
 export class SpringDslEditorLibModule { }
