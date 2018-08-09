c157d22130558d5fe681d82dc014526bff6f752c
Janne Valkealahti <janne.valkealahti@gmail.com>
2018-08-09 17:26:09 +0100

    add some stash code

--- spring-dsl-samples/angular/spring-dsl-editor-lib/projects/spring-dsl-editor/src/lib/spring-dsl-editor.component.ts
+++ spring-dsl-samples/angular/spring-dsl-editor-lib/projects/spring-dsl-editor/src/lib/spring-dsl-editor.component.ts
@@ -1,5 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import { listen, MessageConnection } from 'vscode-ws-jsonrpc';
+import {
+  BaseLanguageClient, CloseAction, ErrorAction,
+  createConnection, MonacoServices, MonacoLanguageClient
+} from 'monaco-languageclient';
+import create = MonacoServices.create;
+
+// import {
+//   BaseLanguageClient, CloseAction, ErrorAction,
+//   createMonacoServices, createConnection
+// } from 'monaco-languageclient';
+
+const normalizeUrl = require('normalize-url');
+
 @Component({
   selector: 'spring-spring-dsl-editor-lib',
   template: `
@@ -11,9 +25,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SpringDslEditorComponent implements OnInit {
 
-  constructor() { }
+  editorOptions = {theme: 'vs', language: 'simple'};
+  code = '';
+  private editor: any;
+
+  constructor() {
+  }
 
   ngOnInit() {
+  }
+
+  initLanguageClient(editor: any) {
+    this.editor = editor;
+    MonacoServices.install(editor);
+    const url = this.createUrl('ws');
+    const webSocket = new WebSocket(url);
+
+    listen({
+      webSocket,
+      onConnection: connection => {
+        // create and start the language client
+        const languageClient = this.createLanguageClient(connection);
+        const disposable = languageClient.start();
+        connection.onClose(() => disposable.dispose());
+      }
+    });
+  }
+
+  private createLanguageClient(connection: MessageConnection): BaseLanguageClient {
+    // const services = createMonacoServices(this.editor);
+    // const services = create(this.editor);
+    return new MonacoLanguageClient({
+      name: 'Sample Language Client',
+      clientOptions: {
+        // use a language id as a document selector
+        documentSelector: ['simple'],
+        synchronize: {
+        },
+        // disable the default error handler
+        errorHandler: {
+          error: () => ErrorAction.Continue,
+          closed: () => CloseAction.DoNotRestart
+        }
+      },
+      // create a language client connection from the JSON RPC connection on demand
+      connectionProvider: {
+        get: (errorHandler, closeHandler) => {
+          return Promise.resolve(createConnection(connection, errorHandler, closeHandler));
+        }
+      }
+    });
+  }
+
+  private createUrl(path: string): string {
+    const protocol = location.protocol === 'https:' ? 'wss' : 'ws';
+    return normalizeUrl(`${protocol}://${location.host}${location.pathname}${path}`);
   }
 
 }
--- spring-dsl-samples/angular/spring-dsl-editor-lib/projects/spring-dsl-editor/src/lib/spring-dsl-editor.module.ts
+++ spring-dsl-samples/angular/spring-dsl-editor-lib/projects/spring-dsl-editor/src/lib/spring-dsl-editor.module.ts
@@ -1,6 +1,7 @@
-import { NgModule } from '@angular/core';
+import { ModuleWithProviders, NgModule } from '@angular/core';
 import { SpringDslEditorComponent } from './spring-dsl-editor.component';
 import { SpringMonacoEditorComponent } from './spring-monaco-editor/spring-monaco-editor.component';
+import { NGX_MONACO_EDITOR_CONFIG, NgxMonacoEditorConfig } from './spring-monaco-editor/config';
 
 @NgModule({
   imports: [
@@ -14,4 +15,13 @@ import { SpringMonacoEditorComponent } from './spring-monaco-editor/spring-monac
     SpringMonacoEditorComponent
   ]
 })
-export class SpringDslEditorModule { }
+export class SpringDslEditorModule {
+  public static forRoot(config: NgxMonacoEditorConfig = {}): ModuleWithProviders {
+    return {
+      ngModule: SpringDslEditorModule,
+      providers: [
+        { provide: NGX_MONACO_EDITOR_CONFIG, useValue: config }
+      ]
+    };
+  }
+}
