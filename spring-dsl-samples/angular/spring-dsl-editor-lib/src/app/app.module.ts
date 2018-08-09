47967f736bf569909b5f2a85e4346c0c2ed60af3
Janne Valkealahti <janne.valkealahti@gmail.com>
2018-08-09 10:30:29 +0100

    Add new lib to test app

--- spring-dsl-samples/angular/spring-dsl-editor-lib/src/app/app.module.ts
+++ spring-dsl-samples/angular/spring-dsl-editor-lib/src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SpringDslEditorLibModule } from 'spring-dsl-editor-lib';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SpringDslEditorLibModule
   ],
   providers: [],
   bootstrap: [AppComponent]
