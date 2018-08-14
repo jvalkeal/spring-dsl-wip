eed0be15543cd86ae6cceafa823c535d8ade81d6
jvalkeal <janne.valkealahti@gmail.com>
2018-08-14 22:03:54 +0300

    tweaks

--- spring-dsl-samples/angular/spring-dsl-editor-lib/projects/spring-dsl-editor/src/lib/spring-dsl-editor.module.ts
+++ spring-dsl-samples/angular/spring-dsl-editor-lib/projects/spring-dsl-editor/src/lib/spring-dsl-editor.module.ts
@@ -1,10 +1,12 @@
 import { ModuleWithProviders, NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { SpringDslEditorComponent } from './spring-dsl-editor.component';
 import { SpringMonacoEditorComponent } from './spring-monaco-editor/spring-monaco-editor.component';
 import { SPRING_DSL_EDITOR_CONFIG, SpringDslEditorConfig } from './spring-monaco-editor/config';
 
 @NgModule({
   imports: [
+    FormsModule
   ],
   declarations: [
     SpringDslEditorComponent,
