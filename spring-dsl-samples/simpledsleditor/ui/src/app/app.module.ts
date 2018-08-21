c5a959f16fed8b6d356007ab7d40101f9873708d
jvalkeal <janne.valkealahti@gmail.com>
2018-08-21 07:20:32 +0300

    reflect changes to editor sample for angular module changes

--- spring-dsl-samples/simpledsleditor/ui/src/app/app.module.ts
+++ spring-dsl-samples/simpledsleditor/ui/src/app/app.module.ts
@@ -2,11 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { SpringDslEditorModule, SpringDslEditorConfig } from 'spring-dsl-editor';
+import { SpringDslEditorModule, SpringMonacoEditorConfig } from 'spring-dsl-editor';
 
-const monacoConfig: SpringDslEditorConfig = {
+const springMonacoEditorConfig: SpringMonacoEditorConfig = {
   onMonacoLoad: () => {
-    console.log((<any>window).monaco);
     (<any>window).monaco.languages.register({ id: 'simple' });
   }
 };
@@ -18,7 +17,7 @@ const monacoConfig: SpringDslEditorConfig = {
   imports: [
     BrowserModule,
     FormsModule,
-    SpringDslEditorModule.forRoot(monacoConfig)
+    SpringDslEditorModule.forRoot(springMonacoEditorConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
